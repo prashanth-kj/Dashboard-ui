218cc01ba721ed2112ad689dcb0ac530f22fe910
prashanth-kj <kjprasanth98@gmail.com>
2023-09-24 21:30:01 +0530

    updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,14 +18,16 @@ function Sidebar() {
                             <ul className="nav nav-pills flex-column">
                               
                                 <li className="nav-item">
+                                <img src='/dashboardicon.svg' alt="dashboardicon" />
                                     <a href="javascript(void)"> 
-                                        <img src='/public/dashboardicon.svg' alt="dashboardicon" />
+                                        
                                         <span >Dashboard</span>
                                     </a>
                                 </li>
                                 <li className="nav-item">
+                                <img src="/labicon.svg" alt="labicon" />
                                    <a href="javascript(void)"> 
-                                       <img src="/public/labicon.svg" alt="labicon" />
+                                       
                                        <span  className='nav1'>Lab Test</span>
                                    </a>
                                </li>
